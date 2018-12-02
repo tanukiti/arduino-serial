6a5dfc8b82b522bd6de9bd2b9e66d4e5651aefa8
kinokoruumu <kinokoruumu416@yahoo.co.jp>
2018-12-02 13:02:26 +0900

    .

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/tarm/serial"
 )
 
 func main() {
+	id := "5bfb97202c4437d772c936e2"
 	fmt.Println("hello world")
-	c := &serial.Config{Name: "/dev/cu.usbmodem14301", Baud: 9600}
+	c := &serial.Config{Name: "/dev/cu.usbmodem1421", Baud: 9600}
 	s, err := serial.OpenPort(c)
 	if err != nil {
 		log.Fatal(err)
@@ -21,8 +23,19 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+		fmt.Println(string(buf[:n]))
 		if string(buf[:n]) != "\r" && string(buf[:n]) != "\n" {
-			log.Printf("%q", buf[:n])
+			if string(buf[:n]) == "0" {
+				fmt.Println("sleep")
+				url := "https://hal-iot.net/iw/nellow/sleep/" + id
+				http.Get(url)
+			}
+
+			if string(buf[:n]) == "1" {
+				fmt.Println("wakeup")
+				url := "https://hal-iot.net/iw/nellow/wakeup/" + id
+				http.Get(url)
+			}
 		}
 	}
 }
